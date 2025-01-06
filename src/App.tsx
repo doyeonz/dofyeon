5f37e9cd67ad50cb5460ba29d3d3b5f9441fedb7
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-06 16:44:37 +0900

    Style: 전역 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Stack } from './stackflow'
+import { GlobalStyle } from './assets/styles/index'
 import '@stackflow/plugin-basic-ui/index.css'
 
-import ReactDom from 'react-dom/client'
-import { Stack } from './stackflow'
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
 
-const root = ReactDom.createRoot(document.getElementById('root')!)
-root.render(<Stack />)
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
+	<React.StrictMode>
+		<GlobalStyle />
+		<Stack />
+	</React.StrictMode>
+)
