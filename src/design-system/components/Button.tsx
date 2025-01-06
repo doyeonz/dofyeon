69c204a55bd784a7a9bb93864055e6b01c30eb3d
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-06 20:16:17 +0900

    Feat: 버튼 컴포넌트

---
+++ src/design-system/components/Button.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import color from '../colors/color'
+
+interface ButtonProps {
+	onClick: () => void
+	children: React.ReactNode
+}
+
+const Button = ({ onClick, children }: ButtonProps) => {
+	return <StyledBtn onClick={onClick}>{children}</StyledBtn>
+}
+
+export default Button
+
+const StyledBtn = styled.button`
+	width: 347px;
+	padding: 14px;
+	text-align: center;
+	border-radius: 12px;
+	border: none;
+	background-color: ${color.primary};
+	color: ${color.gray100}
+	font-size: 16px;
+	font-weight: 700;
+	cursor: pointer;
+	margin-bottom: 70px;
+
+	&:hover {
+		opacity: 0.9;
+	}
+`
