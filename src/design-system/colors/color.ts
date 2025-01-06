21e144a0c7555a2b694167d20cfe003ccf14af2d
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-01-06 12:25:51 +0900

    Style: color system 추가

---
+++ src/design-system/colors/color.ts
@@ -0,0 +1,23 @@
+const color = {
+	black: '#000000',
+	white: '#FFFFFF',
+	orange: '#FF763B',
+	red: '#F44336',
+	green: '#00BF40',
+
+	primary: '#61FF11',
+	secondary: '#6E46FF',
+
+	gray10: '#F2F2F2',
+	gray20: '#E5E5E6',
+	gray30: '#CBCBCD',
+	gray40: '#B1B1B4',
+	gray50: '#97979B',
+	gray60: '#646468',
+	gray70: '#4B4B4E',
+	gray80: '#3F3F41',
+	gray90: '#323234',
+	gray100: '#19191A',
+}
+
+export default color
